9ccd55d5b5cc3d493266a6bf5b417899a85785d0
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-02-14 19:26:17 +0100

    Add files via upload

---
+++ directoriosOcultos.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#Francisco Bayo & Elisa Ortega
+NOMBREDIR=$1
+VISIBILIDAD=$2
+OBJETOACREAR=$3
+CANTIDAD=$4
+
+if [ $# -eq 4 ] && [ $CANTIDAD -ge 1 ] && [ $CANTIDAD -le 100 ];
+then
+    if ! test -t $NOMBREDIR 
+    then
+        if [ $VISIBILIDAD == '-o' ];
+        then
+            echo 'es oculto'
+            mkdir .$NOMBREDIR
+            NOMBREDIR=.$NOMBREDIR
+        elif [ $VISIBILIDAD == '-v' ];
+        then
+            echo 'es visible'
+            mkdir $NOMBREDIR
+        fi
+    else
+        if [ $VISIBILIDAD == '-o' ];
+        then
+            echo 'es oculto'
+            mvdir $NOMBREDIR .$NOMBREDIR
+            NOMBREDIR=.$NOMBREDIR
+        elif [ $VISIBILIDAD == '-v' ];
+        then
+            echo 'es visible'
+            mvdir .$NOMBREDIR $NOMBREDIR
+        fi
+    fi
+
+    if [ $OBJETOACREAR == '-f' ];
+    then
+        for ((i=1;i<=$CANTIDAD;i++)); do
+            touch $NOMBREDIR/file_$NOMBREDIR$i.txt
+            done
+    elif [ $OBJETOACREAR == '-d' ];
+    then
+        for ((i=1;i<=$CANTIDAD;i++)); do
+            mkdir $NOMBREDIR/dir_$NOMBREDIR$i
+            done
+        fi
+fi
