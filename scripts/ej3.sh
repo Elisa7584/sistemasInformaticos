3fa5db3b4f87b050353fafb5261fc833ca1001dc
Elisa7584 <elisaortega112.alu@stemgranada.com>
2024-06-11 22:29:36 +0200

    .

--- scripts/ej3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Eliminar el primer argumento utilizando 'shift'
-nombre1=$1
-shift
-nombre2=$2
-shift
-nombre3=$3
-shift
--- scripts/scriptWhile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-nombre=0
-
-while true;
-do 
-    ls /bin > $nombre
-    nombre=$[ $nombre * 1 ]
-done
